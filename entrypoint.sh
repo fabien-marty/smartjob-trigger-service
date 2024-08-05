fa5ca95e199750a5f79efd2248c6b68a9d47de92
Fabien MARTY <fabien.marty@botify.com>
2024-08-05 13:37:10 +0200

    fix usage as cloudrun service

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,4 @@
 export PATH="${POETRY_HOME}/bin:${PATH}"
 
 cd /app || exit 1
-exec poetry run fastapi run smartjob_trigger_service/main.py
+exec poetry run fastapi run --port "${PORT:-8080}" smartjob_trigger_service/main.py
