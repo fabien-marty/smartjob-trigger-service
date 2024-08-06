7bbd334b8ae13fd1106896c5b6f999d7c26e23b8
Fabien MARTY <fabien.marty@botify.com>
2024-08-06 14:56:27 +0200

    first version

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
 # smartjob-trigger-service
-Cloudrun service to get a bridge between GCP/eventarc and Cloud Run Job
+
+## What is it?
+
+This is a **generic** GCP/Cloud Run Service (docker image):
+
+- to receive GCP/EventArc notifications about new objects created in a bucket (event type: `google.cloud.storage.object.v1.finalized`)
+- and to launch a GCP/Cloud Run Job with the [smartjob](https://github.com/fabien-marty/smartjob) library
+
+## Features
+
+- download the object that triggered the event and inject it in the Cloud Run Job as an input parameter (thanks to the [smartjob](https://githiub.com/fabien-marty/smartjob) library) under the name `incoming_file`
+- can wait for the job completion or just schedule the execution
+
+## Installation / configuration
+
+1️⃣ Create a Cloud Run Service with following parameters:
+
+- docker image: `docker.io/fabienmarty/smartjob-trigger-service:latest`
+- environment variables:
+    - `SMARTJOB_PROJECT=your-gcp-project` **(mandatory)**
+    - `SMARTJOB_REGION=your-gcp-region` **(mandatory)**
+    - `SMARTJOB_STAGING_BUCKET=gs://a-bucket-for-hosting-inputs-outputs-of-the-job` **(mandatory)**
+    - `SMARTJOB_DOCKER_IMAGE=full-path-of-a-docker-image` **(mandatory)** (example: `docker.io/python:3.12`)
+    - `SMARTJOB_LOG_LEVEL=INFO` *(optional, log level of the service, default to `INFO`)*
+    - `SMARTJOB_TIMEOUT_SECONDS=number-of-seconds` *(optional, timeout of the job in seconds, default to `3600`)*
+    - `SMARTJOB_SERVICE_ACCOUNT=service-account-email` *(optional, service account to use for the job, default to GCP default)*
+    - `SMARTJOB_CPU=1.0` *(optional, number of CPUs required for the job, default to `1.0`, can be fractional)*
+    - `SMARTJOB_MEMORY_GB=0.5` *(optional, number of GB of memory required for the job, default to `0.5`, can be fractional)*
+
+2️⃣ Create an EventArc configuration with following parameters:
+
+- Trigger type: `Google sources`
+- Event provider: `Cloud Storage`
+- Event type: `google.cloud.storage.objects.v1.finalized` **(at this time, this is the only event type supported)**
+- Event data content type: `application/json`
+- Bucket: `your-bucket-that-will-trigger-the-job` **(WARNING: don't use here the "staging bucket" to avoid an infinite loop!)**
+- Event destination: `Cloud Run`
+- Cloud Run Service: `your-cloudrun-service-previously-created`
+- Service URL path:
+    - `/schedule/{job-namespace}/{job-name}` for triggering the job without waiting for completion (probably the one you want)
+    - or `/run/{job-namespace}/{job-name}` for running the job and wait for its completion (warning: you have to increase the default "acknowledgement deadline" (default to 10 seconds) but as you can't increase it over 600 seconds, you can't use this option for long running jobs)
+
+> [!WARNING]  
+> The default EventArc "acknowledgement deadline" (you can find it in the automatically generated PubSub configuration in the "subscription" part of the configuration) is very low by default: 10 seconds!
+>
+> - For the "run and wait" feature (`/run/...`), it's clearly too low! But note that you can't increase it over 600 seconds, so you can't use this feature for long running jobs.
+> - For the "schedule only"  feature (`schedule/...`), it can be too low, increase it at its maximum to be safe
--- smartjob_trigger_service/main.py
+++ smartjob_trigger_service/main.py
@@ -1,8 +1,43 @@
 import json
+import os
+from contextlib import asynccontextmanager
 
-from fastapi import FastAPI, Request
+import stlog
+from fastapi import FastAPI, HTTPException, Request
+from smartjob import CloudRunSmartJob, GcsInput, Input, get_executor_service_singleton
 
-app = FastAPI()
+
+def check_env_or_raise():
+    for env in (
+        "SMARTJOB_PROJECT",
+        "SMARTJOB_REGION",
+        "SMARTJOB_STAGING_BUCKET",
+        "SMARTJOB_DOCKER_IMAGE",
+    ):
+        if env not in os.environ:
+            raise Exception(f"Environment variable '{env}' not found")
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    stlog.setup(level=os.environ.get("SMARTJOB_LOG_LEVEL", "INFO"))
+    check_env_or_raise()
+    yield
+
+
+def get_smartjob_cpu_from_env(e: str | None) -> float:
+    if e is None:
+        return 1.0
+    return float(e)
+
+
+def get_smartjob_memory_gb_from_env(e: str | None) -> float:
+    if e is None:
+        return 0.5
+    return float(e)
+
+
+app = FastAPI(lifespan=lifespan)
 
 
 @app.get("/")
@@ -10,8 +45,81 @@ async def hello():
     return {"message": "Hello World"}
 
 
-@app.post("/process")
-async def process(request: Request):
-    body = await request.json()
-    print(json.dumps(body, indent=4))
-    return {"message": "Processing job"}
+async def get_job_and_input(
+    request: Request, namespace: str, name: str
+) -> tuple[CloudRunSmartJob, Input]:
+    logger = stlog.getLogger("smartjob-trigger-service")
+    try:
+        body = await request.json()
+    except json.decoder.JSONDecodeError:
+        raise HTTPException(status_code=400, detail="Invalid JSON body")
+    logger.debug("received body: %s" % json.dumps(body, indent=4))
+    for prop in ("kind", "id", "bucket"):
+        if prop not in body:
+            raise HTTPException(
+                status_code=400, detail=f"'{prop}' property not found in the JSON body"
+            )
+    if body["kind"] != "storage#object":
+        raise HTTPException(
+            status_code=400, detail="The 'kind' property must be 'storage#object'"
+        )
+    id = body["id"]
+    bucket = body["bucket"]
+    if not id.startswith(bucket):
+        raise HTTPException(
+            status_code=400,
+            detail="The 'id' property must start with the 'bucket' property",
+        )
+    path = id[len(body["bucket"]) + 1 :]  # +1 because we want also to remove the slash
+    gcs_path = f"gs://{bucket}/{path}"
+    logger.debug("gcs input path: %s" % gcs_path)
+    input = GcsInput(filename="incoming_file", gcs_path=gcs_path)
+    job = CloudRunSmartJob(
+        name=name,
+        namespace=namespace,
+        docker_image=os.environ["SMARTJOB_DOCKER_IMAGE"],
+        timeout_seconds=int(os.environ.get("SMARTJOB_TIMEOUT_SECONDS", 3600)),
+        service_account=os.environ.get("SMARTJOB_SERVICE_ACCOUNT"),
+        cpu=get_smartjob_cpu_from_env(os.environ.get("SMARTJOB_CPU")),
+        memory_gb=get_smartjob_memory_gb_from_env(os.environ.get("SMARTJOB_MEMORY_GB")),
+    )
+    return job, input
+
+
+@app.post("/schedule/{namespace}/{name}")
+async def schedule(request: Request, namespace: str, name: str):
+    logger = stlog.getLogger("smartjob-trigger-service")
+    logger.info("received schedule request")
+    executor_service = get_executor_service_singleton()
+    job, input = await get_job_and_input(request, namespace, name)
+    future = await executor_service.schedule(job, inputs=[input])
+    execution_id = future.execution_id
+    log_url = future.log_url
+    future._cancel()  # we don't want to be notified about the job completion
+    return {
+        "message": "job scheduled",
+        "execution_id": execution_id,
+        "log_url": log_url,
+    }
+
+
+@app.post("/run/{namespace}/{name}")
+async def run(request: Request, namespace: str, name: str):
+    logger = stlog.getLogger("smartjob-trigger-service")
+    logger.info("received run request")
+    executor_service = get_executor_service_singleton()
+    job, input = await get_job_and_input(request, namespace, name)
+    result = await executor_service.run(job, inputs=[input])
+    if not result:
+        raise HTTPException(
+            status_code=500, detail="Job launch failed, job_log_url=%s" % result.log_url
+        )
+    execution_id = result.execution_id
+    log_url = result.log_url
+    if result.json_output is not None:
+        logger.info("job output: %s" % json.dumps(result.json_output, indent=4))
+    return {
+        "message": "job run successfully",
+        "execution_id": execution_id,
+        "log_url": log_url,
+    }
