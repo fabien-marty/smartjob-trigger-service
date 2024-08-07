9cb6d568aaf80b82a778f55b7390b1334543b14b
Fabien MARTY <fabien.marty@botify.com>
2024-08-07 17:28:32 +0200

    add storage.objects.create support

--- smartjob_trigger_service/main.py
+++ smartjob_trigger_service/main.py
@@ -45,25 +45,6 @@ async def hello():
     return {"message": "Hello World"}
 
 
-def get_gcs_path_from_body(body: dict) -> str:
-    id = body["id"]
-    bucket = body["bucket"]
-    generation = body["generation"]
-    if not id.startswith(bucket):
-        raise HTTPException(
-            status_code=400,
-            detail="The 'id' property must start with the 'bucket' property",
-        )
-    if not id.endswith(f"/{generation}"):
-        raise HTTPException(
-            status_code=400,
-            detail="The 'id' property must end with the 'generation' property",
-        )
-    tmp = id[len(bucket) + 1 :]  # +1 because we want also to remove the slash
-    path = tmp[: -len(f"/{generation}")]
-    return f"gs://{bucket}/{path}"
-
-
 async def get_job_and_input(
     request: Request, namespace: str, name: str
 ) -> tuple[CloudRunSmartJob, Input]:
@@ -73,6 +54,87 @@ async def get_job_and_input(
     except json.decoder.JSONDecodeError:
         raise HTTPException(status_code=400, detail="Invalid JSON body")
     logger.debug("received body: %s" % json.dumps(body, indent=4))
+    if body.get("kind", "") == "storage#object":
+        return get_job_and_input_from_finalized_event(namespace, name, body)
+    elif (
+        body.get("@type", "")
+        == "type.googleapis.com/google.events.cloud.audit.v1.LogEntryData"
+    ):
+        return get_job_and_input_from_create_event(namespace, name, body)
+    else:
+        raise HTTPException(
+            status_code=400,
+            detail="The JSON body must be a storage object finalized event"
+            " or a cloud audit log entry",
+        )
+
+
+def get_job_and_input_from_create_event(
+    namespace: str, name: str, body: dict
+) -> tuple[CloudRunSmartJob, Input]:
+    logger = stlog.getLogger("smartjob-trigger-service")
+    if "protoPayload" not in body:
+        raise HTTPException(
+            status_code=400, detail="'protoPayload' property not found in the JSON body"
+        )
+    proto_payload = body["protoPayload"]
+    if "resourceName" not in proto_payload:
+        raise HTTPException(
+            status_code=400,
+            detail="'resourceName' property not found in the JSON body/protoPayload",
+        )
+    resource_name = proto_payload["resourceName"]
+    # projects/_/buckets/fabien2/objects/copilot mvp.png",
+    if not resource_name.startswith("projects/_/buckets/"):
+        raise HTTPException(
+            status_code=400,
+            detail="The 'resourceName' property must start with 'projects/_/buckets/'",
+        )
+    tmp = resource_name.split("/", 5)
+    if len(tmp) != 6:
+        raise HTTPException(status_code=400, detail="bad resourceName format")
+    bucket = tmp[3]
+    path = tmp[5]
+    gcs_path = f"gs://{bucket}/{path}"
+    logger.debug("gcs input path: %s" % gcs_path)
+    input = GcsInput(filename="incoming_file", gcs_path=gcs_path)
+    job = CloudRunSmartJob(
+        name=name,
+        namespace=namespace,
+        docker_image=os.environ["SMARTJOB_DOCKER_IMAGE"],
+        timeout_seconds=int(os.environ.get("SMARTJOB_TIMEOUT_SECONDS", 3600)),
+        service_account=os.environ.get("SMARTJOB_SERVICE_ACCOUNT"),
+        cpu=get_smartjob_cpu_from_env(os.environ.get("SMARTJOB_CPU")),
+        memory_gb=get_smartjob_memory_gb_from_env(os.environ.get("SMARTJOB_MEMORY_GB")),
+        add_envs={
+            "SMARTJOB_TRIGGER_SERVICE_FULL_PATH": gcs_path,
+        },
+    )
+    return job, input
+
+
+def get_job_and_input_from_finalized_event(
+    namespace: str, name: str, body: dict
+) -> tuple[CloudRunSmartJob, Input]:
+    def get_gcs_path_from_body(body: dict) -> str:
+        id = body["id"]
+        bucket = body["bucket"]
+        generation = body["generation"]
+        if not id.startswith(bucket):
+            raise HTTPException(
+                status_code=400,
+                detail="The 'id' property must start with the 'bucket' property",
+            )
+        if not id.endswith(f"/{generation}"):
+            raise HTTPException(
+                status_code=400,
+                detail="The 'id' property must end with the 'generation' property",
+            )
+        tmp = id[len(bucket) + 1 :]  # +1 because we want also to remove the slash
+        path = tmp[: -len(f"/{generation}")]
+        return f"gs://{bucket}/{path}"
+
+    logger = stlog.getLogger("smartjob-trigger-service")
     for prop in ("kind", "id", "bucket", "generation"):
         if prop not in body:
             raise HTTPException(
