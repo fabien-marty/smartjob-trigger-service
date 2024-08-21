8015996b162d31b035122d9139840e7ef558b481
Fabien MARTY <fabien.marty@botify.com>
2024-08-21 09:47:22 +0200

    use new stlog version to get GCP logs

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,10 @@
 
 export PATH="${POETRY_HOME}/bin:${PATH}"
 
+if test "${K_SERVICE:-}" != ""; then
+    echo "CloudRun env detected"
+    export STLOG_OUTPUT="json-gcp" # change log format
+fi
+
 cd /app || exit 1
 exec poetry run fastapi run --port "${PORT:-8080}" smartjob_trigger_service/main.py
