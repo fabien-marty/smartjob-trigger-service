cf7ad8ae5ebe8b82cf4f29f06e2eef5656430453
Fabien MARTY <121722087+fabien-marty@users.noreply.github.com>
2025-05-19 12:09:48 +0200

    add sidecar support (#8)

--- smartjob_trigger_service/main.py
+++ smartjob_trigger_service/main.py
@@ -69,6 +69,11 @@ execution_config = ExecutionConfig(
     },
     vpc_connector_network=os.environ.get("SMARTJOB_VPC_CONNECTOR_NETWORK"),
     vpc_connector_subnetwork=os.environ.get("SMARTJOB_VPC_CONNECTOR_SUBNETWORK"),
+    sidecars_container_images=[
+        x.strip()
+        for x in os.environ.get("SMARTJOB_SIDECARS_CONTAINER_IMAGES", "").split(",")
+        if x.strip()
+    ],
 )
 
 executor = os.environ.get("SMARTJOB_EXECUTOR", "cloudrun")
